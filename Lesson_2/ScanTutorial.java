44d207bb08909b491fa2584676c6e917b8964451
CptMonac <ayobeko@gmail.com>
2016-08-13 12:40:50 -0700

    Update ScanTutorial.java

--- Lesson_2/ScanTutorial.java
+++ Lesson_2/ScanTutorial.java
@@ -26,10 +26,6 @@ import java.awt.*;
     
     Additional reference information and documentation is available in the 'javadoc' folder under 'Robocode'
 */
-// This lesson will teach you how to use the radar to scan the battlefield. The scanner is the fastest moving part of your robot, so it's important to learn how to use it well.
-// Your robot has a few senses: sense of touch, sense of sight, meta senses
-// Sense of touch: Your robot knows when it's hit a wall, been hit by a bullet, or if it hit another robot
-// Meta senses: Your robot knows when it's died, or when another robot has died. It also knows if a bullet hits a wall or another bullet.
 
 public class ScanTutorial extends Robot
 {
@@ -43,30 +39,16 @@ public class ScanTutorial extends Robot
 			Reference: The 'run' function is the function that tells your robot what to do, and how to dress.
 		*/
 
-		/*
-       		Setting the robot's colors.
-        
-            Reference: setColors(<body color>, <gun color>, <radar color>, <bullet color>, <radar-scan color>)
-                   This function sets the colors for your robot's body, gun, radar, bullets, and radar-scans.
-            E.g. setColors(orange, red, yellow, blue, pink) -> orange body, red gun, yellow radar, blue bullets, pink radar-scans
-    	*/
-		setColors(Color.black, Color.blue, Color.orange, Color.white, Color.red);
+		setColors(Color.black, Color.blue, Color.orange, Color.white, Color.red); //Set robot's colors 
 
 		while(true)
 		{
-			/*
+		/*
 	          Reference: The 'while' function allows the 'run' function above to run repeatedly. If you didn't 
 	                      include it here, your robot would only do what you told it to do once, and then stop
       		*/
-			
-	        /*
-				#1 -- Move the radar in a 360 degree scan around your robot
-
-				Reference: turnRadarRight(<no. of degrees>)
-							This function turns the radar to the right by the number of degrees you specify
-						E.g. turnRadarRight(20) -> Move the radar in a 20 degree scan around the robot
-	        */
-			turnRadarRight(360);
+	
+			turnRadarRight(360); //Move the radar in a 360 degree scan around your robot
 		}
 	}
 
@@ -79,57 +61,13 @@ public class ScanTutorial extends Robot
 		double enemyRadarAngle;		//This variable will store the angle to turn the radar to lock onto the enemy robot
 		double enemyGunAngle;		//This variable will store the angle to turn the gun to lock onto the enemy robot
 		
-		/*
-			#2.1 -- Let's lock the radar to the enemy robot's position
-				  	Store the angle to turn the radar to face the enemy in the 'enemyRadarAngle' variable
-
-			Reference example: magicJohnson = 32;
- 					In this example, you store the number 32 in the magicJohnson variable.
-
-			Reference: getEnemyRadarHeading(enemyRobot)
-					This function returns the angle that the radar should turn to lock onto the enemy robot you specify
-					E.g. getEnemyRadarHeading(enemyRobot) -> returns 10 degrees to lock onto enemyRobot 			
-		*/
-		enemyRadarAngle = getEnemyRadarHeading(enemyRobot);
-
-		/*
-			#2.2 -- Turn your radar to face the angle that the enemy robot was spotted. 
-					This will be the angle in your 'enemyRadarAngle' variable above
-		*/
-		turnRadarRight(1.9*enemyRadarAngle);
+		enemyRadarAngle = getEnemyRadarHeading(enemyRobot); //Store the angle to turn the radar to face the enemy in the 'enemyRadarAngle' variable
+		turnRadarRight(1.9*enemyRadarAngle); //Turn your radar to face the angle that the enemy robot was spotted. 
 		
-		
-		/*
-			#3.1 -- Let's lock the gun of the robot to the enemy robot's position. Otherwise, the robot will shoot in the wrong direction
-					Store the angle the angle to turn the gun to face the enemy in the 'enemyGunAngle' variable
-			
-			Reference: getEnemyGunHeading(enemyRobot)
-					This function returns the angle that the gun should turn to face the enemy robot you specify
-					E.g. getEnemyGunHeading(enemyRobot) -> returns 20 degrees to face the enemyRobot
-		*/
-		enemyGunAngle = getEnemyGunHeading(enemyRobot);
+		enemyGunAngle = getEnemyGunHeading(enemyRobot); //Store the angle the angle to turn the gun to face the enemy in the 'enemyGunAngle' variable
+		turnGunRight(enemyGunAngle); //Turn your gun to face the angle that the enemy robot was spotted.
 
-		/*
-			#3.2 -- Turn your gun to face the angle that the enemy robot was spotted.
-					This will be the angle in your 'enemyGunAngle' variable above.
-
-			Reference: turnGunRight(<no. of degrees>)
-						This function turns the gun to the right by the number of degrees you specify
-					E.g. turnGunRight(20) -> Turn the gun right by 20 degrees
-
-		*/
-		turnGunRight(enemyGunAngle);
-
-
-		/*
-			#4 -- Fire an energy bullet of power 2 at the enemy robot
-
-			Reference: fire(<no.>)
-						This function will immediately fire a bullet with power equal to the number you specify in the direction the gun is pointing.
-						You can only specify a number between 1 and 3. The higher numbers you specify, the longer the gun takes to recharge before it can fire again.
-				E.g. fire(2) -> fires a bullet with power 2 at the enemy robot
-		*/
-		fire(2);
+		fire(2); //Fire an energy bullet of power 2 at the enemy robot
 	}
 
 	public void onHitByBullet(HitByBulletEvent bulletEvent)
@@ -138,23 +76,8 @@ public class ScanTutorial extends Robot
 			Reference: The 'onHitByBullet' function tells the robot what to do when it's hit by a bullet
 		*/
 
-		/* 
-			#5 -- If the robot is hit by a bullet, tell the robot to do the following:
-				  Turn right by 45 degrees
-				  Move ahead by 60px
-			
-			Reference: turnRight(<no. of degrees>)
-                     This function turns the robot right by the number of degrees you specify.
-                     E.g. turnRight(12) -> turn the robot right by 12 degrees 
-
-            Reference: ahead(<no. of pixels>)
-                     This function moves the robot ahead by the number of pixels you specify.
-                     E.g. ahead(500) -> move the robot ahead by 500 pixels
-
-		*/
-
-		turnRight(45);
-		ahead(60);
+		turnRight(45); //Turn right by 45 degrees
+		ahead(60); //Move ahead by 60px
 
 	}
 	
@@ -164,22 +87,8 @@ public class ScanTutorial extends Robot
 			Reference: The 'onHitWall' function tells the robot what to do when it hit a wall
 		*/
 
-		/* 
-			#6 -- If the robot hit a wall, tell the robot to do the following:
-				  Turn left by 20 degrees
-				  Move back by 10px
-
-			Reference: turnLeft(<no. of degrees>)
-	                This function turns your robot left by the number of degrees you specify.
-	                E.g. turnleft(12) -> turns the robot left by 12 degrees 
-
-	        Reference: back(<no. of pixels>)
-                    This function moves the robot backward by the number of pixels you specify.
-                    E.g. back(500) -> move the robot backward by 500 pixels
-
-		*/
-        turnLeft(20);
-        back(10);
+		turnLeft(20);  // Turn left by 20 degrees
+        	back(10); //Move back by 10px
 	}
 
 	public void onBulletHit(BulletHitEvent bulletEvent)
